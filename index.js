6b60cee5b23cd793a60d6688cb6c10ab222f2084
Oliver Anteros <rift95@live.se>
2018-08-07 12:19:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+Array.zip = (...args) => {
+	const lastArg = args.pop();
+	const zipper = typeof lastArg === 'function'
+  	? lastArg : (...args) => args;
+  const arrays = typeof lastArg === 'function'
+  	? args : [...args, lastArg];
+  const maxLength = Math.max(...arrays.map(arr => arr.length));
+  return Array(maxLength).fill().map((_, i) => zipper(...arrays.map(arr => arr[i])));
+}
